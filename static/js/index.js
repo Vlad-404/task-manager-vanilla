9c87ced04cc5dd7895bb094ed5644d06dbe16c54
Vlad-404 <vmijat21@gmail.com>
2021-02-24 16:39:43 +0100

    Version 1 completed(see version-controll.txt)

---
+++ static/js/index.js
@@ -0,0 +1,18 @@
+const tasksTable = document.getElementById('tasks-table');
+
+let todos = [
+    {
+        taskText: 'First Task',
+        completed: true
+    },
+    {
+        taskText: 'Second Task',
+        completed: false
+    },
+    {
+        taskText: 'Third Task',
+        completed: false
+    }
+]
+
+console.log(todos);
