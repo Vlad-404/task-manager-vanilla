7bd5b34fe860fd89d98702757225a8e40eefbf72
Vlad-404 <vmijat21@gmail.com>
2021-02-24 18:21:58 +0100

    Ver4: added task rendering with JS

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 const tasksTable = document.getElementById('tasks-table');
 
-let todos = [
+let tasks = [
     {
         taskText: 'First Task',
         completed: true
@@ -15,4 +15,24 @@ let todos = [
     }
 ]
 
-// console.log(todos);
+showTasks = () => {
+    tasksTable.innerHTML = '';
+    tasksTable.classList.add('mb-4');
+
+    for ( let i=0; i < tasks.length; i++) {
+        var task = tasksTable.insertRow(i)
+        task.id = 'task-' + i
+        task.innerHTML = `
+            <td><input type="checkbox"></td>
+            <td class="text-left">${tasks[i].taskText}</td>
+            <td><button class="btn-blue">Edit</button></td>
+            <td><button class="btn-red">Remove</button></td>
+        `
+    }
+    // console.log(tasksTable);
+}
+
+const refreshBtn = document.getElementById('refresh');
+refreshBtn.addEventListener('click', showTasks);
+
+showTasks();
